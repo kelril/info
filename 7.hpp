6fb267c2ddba2e50f54980b46acecfbaf5e7888d
kelril <kelril@dsu1.service.sarnet>
2020-04-26 19:16:49 +0300

    commit 7.1.5

--- 7.hpp
+++ 7.hpp
@@ -22,27 +22,35 @@
 #include <boost/log/utility/setup/console.hpp>
 #include <boost/log/expressions.hpp>
 
+//  mutex - это взамное исключение, базовый механизм синхранизации.
+// Предназначен для организации взаиносключающего доступа к общим данным
+// для нескольких потоков с использованием барьеров памяти
+
+//  Синронная модель - потоку назначается одна задача и начинается выполнение.
+// Когда завершено выполнение появляется возможность заниматься другой задачей
+// МИНУСЫ: невозможно остановить выполнение задачи, чтобы в промежутке выполнить другую задачу.
+
+//  Асинхронная модель - в отличии от синхр. модели, тут поток может приостоновиться,
+// и выполнить другую задачу, а потом вернуться к предыдущей.
+
 namespace logging = boost::log;
 namespace keywords = boost::log::keywords;
 
 #define MAX_MSG 1024
 
-
-//using namespace boost::asio;
-
 using std::cout;
 using std::endl;
 
-
-boost::asio::io_service service; // создание экземпляра io_service для общения с сервисом ввода/вывода ОС
+// Создаем экземпляр для общения с сервисом ввода/вывода ОС
+boost::asio::io_service service;
 boost::recursive_mutex mx;
 
 class Server;
 
-// Вектор клиентов
-std::vector<std::shared_ptr<Server>> clients; //нужен список клиентов, чтобы обрабатывать входящие запросы от них.
+std::vector<std::shared_ptr<Server>> clients; // Вектор клиентов
 
-class Server {
+class Server
+{
 private:
     boost::asio::ip::tcp::socket sock;
     int already_read_;
@@ -54,44 +62,43 @@ private:
 public:
     Server() : sock(service), clients_changed_(false) {}
 
-    boost::asio::ip::tcp::socket &sock_r() {
-        return sock;
+    boost::asio::ip::tcp::socket &sock_r()
+    {
+        return sock; //ретёрн сокета 219 строка. Занесение сокета в класс
     }
 
-    std::string username() const {
+    std::string username() const
+    {
         return username_;
     }
 
-    void answer_to_client() {   // Ответ клиенту
-        try {
-            read_request();     // Чтение запроса //Чтение будет происходить только если есть данные, таким образом, сервер никогда не будет заблокирован
-            process_request();  //Обработка запроса
+    void answer_to_client() // Ответ клиенту
+    {
+        try
+        {
+            read_request();    // Чтение запроса
+            process_request(); // Обработка запроса
         }
-        // Обработка ошибки, которая может произойти в блоке try
-        catch (boost::system::system_error &) {
+
+        catch (boost::system::system_error &) // Обработка ошибки, которая может произойти в блоке try
+        {
             stop(); // Выключение сервера
         }
 
-        // Провека на время
-        // Если клиент не пингукется в теченнии 5 сек, то кикнуть его
-        if (timed_out())
-            stop();
+        if (timed_out()) // Провека на время
+            stop();      // Если клиент не пингукется в теченнии 5 сек, то кикнуть его
     }
 
-    void read_request() {   // Чтение запроса
-        if (sock.available()) //если сокет доступен
-            already_read_ += sock.read_some(boost::asio::buffer
-                    (buff_ + already_read_, MAX_MSG - already_read_)); //в buff_ сохраняет запрос,который не больше 1024 символов
+    void read_request()
+    { // Чтение запроса в том случае, когда есть данные.
+        if (sock.available())
+            already_read_ += sock.read_some(boost::asio::buffer(buff_ + already_read_, MAX_MSG - already_read_));
     }
 
-    void process_request() {    // Обработка полученого запроса
-        // После того как мы считали те данные, которые были доступны, мы должны проверить
-        // считали ли мы сообщение до конца (если да, то found_enteris установится в true).
-        // Если это так, то мы защищаем себя от чтения больше чем одного сообщения
-        // (после символа ‘\n’ сохраняться в буфер ничего не будет),
-        // а затем мы интерпретируем полностью прочитанное сообщение
-        bool found_enter = std::find(buff_, buff_ + already_read_, '\n')
-                < buff_ + already_read_; // проверка считали ли мы сообщение до конца
+    void process_request()
+    {   // Обработка полученого запроса: (1) защита от чтения больше 1-го сообщения,
+        // (2) проверка считали ли мы до конца сообщение
+        bool found_enter = std::find(buff_, buff_ + already_read_, '\n') < buff_ + already_read_;
         if (!found_enter)
             return;
 
@@ -104,28 +111,32 @@ public:
         std::copy(buff_ + already_read_, buff_ + MAX_MSG, buff_);
         already_read_ -= pos + 1;
 
-        if (msg.find("login ") == 0) on_login(msg); //индекс с которого начинается подстрока  // Регистрация пользователя
-        else if (msg.find("ping") == 0) on_ping();
-        else if (msg.find("ask_clients") == 0) on_clients(); //ответ клиенту
+        if (msg.find("login ") == 0)
+            on_login(msg); // Метод поиска подроки в строке(login). На нулевом месте.
+        else if (msg.find("ping") == 0)
+            on_ping();
+        else if (msg.find("ask_clients") == 0)
+            on_clients();
         else
-            std::cerr << "invalid msg " << msg << std::endl; //поток вывода ошибок
+            std::cerr << "invalid msg " << msg << std::endl; // Поток вывода ошибок
     }
 
-    void on_login(const std::string &msg) {    // Регистрация пользователя
-        std::istringstream in(msg); //stringstream позволяет связать поток ввода-вывода со строкой в памяти
-        in >> username_ >> username_;
-        //std::cout<< username_ <<std::endl;
-        write("login ok\n"); // в сокет записывается выражение
+    void on_login(const std::string &msg)
+    {                                 // Регистрация пользователя
+        std::istringstream in(msg);   // Связывание потока сос трокой
+        in >> username_ >> username_; // занос в приват
+        write("login ok\n");
         {
-            // Потокобезопасный доступ к вектору клиентов
-            boost::recursive_mutex::scoped_lock lk(mx); //чтобы потоки не могли одновременно работать с одними данными
-            for (unsigned i = 0; i < clients.size() - 1; i++) // Каждый клиент выставляет флаг
-                clients[i]->update_clients_changed();
+            boost::recursive_mutex::scoped_lock lk(mx);
+            // Каждый клиент выставляет флаг
+            for (unsigned i = 0; i < clients.size() - 1; i++)
+                clients[i]->update_clients_changed(); // установка флага в векторе единицой.
         }
     }
 
     // Обновление списка клиентов (выставление флага)
-    void update_clients_changed() {
+    void update_clients_changed()
+    {
         clients_changed_ = true;
     }
 
@@ -135,100 +146,102 @@ public:
     // либо client_list_chaned
     // (в последнем случае клиент повторно запрашивает список
     // подключенных клиентов);
-    void on_ping() {
-        //std::cout<<clients_changed_<<std::endl;
-        write(clients_changed_ ? "ping client_list_changed\n" : "ping ok\n");//если список клиентов обновился, то выводится первое, если нет второе
-        clients_changed_ = false;   // Сброс флага
+    void on_ping()
+    {
+        write(clients_changed_ ? "ping client_list_changed\n" : "ping ok\n"); //true or falls
+        clients_changed_ = false;                                             // Сброс флага
     }
 
-    void on_clients() { // Ответ клиенту
+    void on_clients() // Ответ клиенту
+    {
         std::string msg;
-    {       // Потокобезопасный доступ к вектору клиентов
-            boost::recursive_mutex::scoped_lock lk(mx); //чтобы потоки не могли одновременно работать с одними данными
+        {
+            boost::recursive_mutex::scoped_lock lk(mx);
             for (auto b = clients.begin(), e = clients.end(); b != e; ++b)
                 msg += (*b)->username() + " ";
         }
         write("clients " + msg + "\n");
     }
 
-    void write(const std::string &msg) {   // Запись сообшениия
+    void write(const std::string &msg)
+    { // Запись сообшениия
         //cout<<msg<<endl;
         sock.write_some(boost::asio::buffer(msg));
     }
 
-    void stop() {   // Закрытие сокета
+    void stop() // Закрытие сокета
+    {
         boost::system::error_code err;
         sock.close(err);
     }
 
-    bool timed_out() const {    // Подсчет времени для отключения
+    bool timed_out() const // Подсчет времени для отключения
+    {
         boost::posix_time::ptime now =
-                boost::posix_time::microsec_clock::local_time();
+            boost::posix_time::microsec_clock::local_time();
         int ms = (now - last_ping).total_milliseconds();
         return ms > 5000;
     }
 };
 
-// Инициализация логов
-static void init_logging() {
-    
-    logging::add_file_log (
-                    keywords::file_name = "info.log",
-                    keywords::format = "[%TimeStamp%] [%ThreadID%] [%Severity%] %Message%"
-            );
+static void init_logging() // Инициализация логов
+{
+    logging::add_file_log(
+        keywords::file_name = "info.log",
+        keywords::format = "[%TimeStamp%] [%ThreadID%] [%Severity%] %Message%");
 
-    logging::add_console_log (
-            std::cout,
-            keywords::format = "[%TimeStamp%] [%ThreadID%] [%Severity%] %Message%"
-    );
+    logging::add_console_log(
+        std::cout,
+        keywords::format = "[%TimeStamp%] [%ThreadID%] [%Severity%] %Message%");
 
     logging::add_common_attributes();
 }
 
-// Поток для прослушивания новых клиентов
-void accept_thread() {
-    // Задаем порт (8001) для прослушивания и создаем акцептор (приемник)
-    // — один объект (service), который принимает клиентские подключения
+void accept_thread() // Поток для прослушивания новых клиентов
+{
+    // Задаем порт для прослушивания и создаем акцептор (приемник)
+    // — один объект, который принимает клиентские подключения
     boost::asio::ip::tcp::acceptor acceptor(service,
-            boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 8001));
-    init_logging(); // Инициализация логирования
+                                            boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 8001));
+    init_logging();
 
-    while (true) {
-        // Создаем  умный указатель cl (на сокет)
-        std::shared_ptr<Server> cl = std::make_shared<Server>();
+    while (true)
+    {
+        std::shared_ptr<Server> cl = std::make_shared<Server>(); // Создаем  умный указатель cl (на сокет)
         std::cout << "wait client" << std::endl;
-        //Принять запрос на установку соединения accept
-        acceptor.accept(cl->sock_r());  // инициализация сокета // Ждем подключение клиента
+        acceptor.accept(cl->sock_r()); // Ждем подключение клиента. Обращаемся к полю и заносим класс в сокет. Вызов функции для подключение. геттер
         BOOST_LOG_TRIVIAL(trace) << "client acepted" << endl;
-        // Потокобезопасный доступ к вектору клиентов
-        boost::recursive_mutex::scoped_lock lk(mx); //чтобы потоки не могли одновременно работать с одними данными
-        clients.push_back(cl);  // Добавление нового клиента в вектор
+        // Потокобезопасный доступ к вектору клииентов
+        boost::recursive_mutex::scoped_lock lk(mx); //взаимоисключающий доступ, для организации потоков.
+        clients.push_back(cl);                      // Добавление нового клиента в вектор. добовление в конец вектора указателя
     }
 }
 
 // Поток для прослушиваниия существующих клиентов
-void handle_clients_thread() {
-    while (true) {
-        boost::this_thread::sleep(boost::posix_time::millisec(1));//очередь потоков
-        // Потокобезопасный доступ к вектору клиентов
-        boost::recursive_mutex::scoped_lock lk(mx);
+void handle_clients_thread()
+{
+    while (true)
+    {
+        boost::this_thread::sleep(boost::posix_time::millisec(1)); // Потокобезопасный доступ к вектору клииентов
+        boost::recursive_mutex::scoped_lock lk(mx);                // Очередь потоков
 
         for (auto b = clients.begin(); b != clients.end(); ++b)
-            (*b)->answer_to_client();   // Отпраляем ответы КАЖДОМУ клиенту
-            //вызываем answer_to_client для каждого клиаента
+            (*b)->answer_to_client(); // Отпраляем ответы КАЖДОМУ клиенту
 
         // Удаляем клиенты, у которых закончилось время
         clients.erase(std::remove_if(clients.begin(), clients.end(),
-                    boost::bind(&Server::timed_out, _1)), clients.end());
+                                     boost::bind(&Server::timed_out, _1)),
+                      clients.end());
     }
 }
 
-
-int main() {
+int main()
+{
     boost::thread_group threads;
-    //т.к accept() блокирующая операция создаем 2 потока
-    threads.create_thread(accept_thread); // Поток для прослушивания новых клиентов
-    threads.create_thread(handle_clients_thread);// Поток для обработки существующих клиентов
+    // Поток для прослушивания новых клиентов
+    threads.create_thread(accept_thread);
+    // Поток для обработки существующих клиентов
+    threads.create_thread(handle_clients_thread);
     // Запуск потоков и ожидание завершения последнего
     threads.join_all();
     return 0;
