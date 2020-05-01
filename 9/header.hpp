02626e3fdb55fc4ec616a343bd06b911d5b4dfb5
kelril <kelril@dsu1.service.sarnet>
2020-05-01 22:15:25 +0300

    add lab 9

--- 9/header.hpp
+++ 9/header.hpp
@@ -39,51 +39,54 @@ struct HrefData {
 
 class Crawler {
 public:
-    explicit Crawler(std::string beginPage = "https://yandex.ru",   // конструктор задает введенные пользователем параметры, либо параметры по умолчанию
+    // конструктор задает введенные пользователем параметры, либо параметры по умолчанию
+   //если параметры не заданы, то устанавливается то, что до :
+    explicit Crawler(std::string beginPage = "https://yandex.ru",
                      uint64_t maxDepth = 1,
                      uint8_t producerThreadsCount = 10,
                      uint8_t consumerThreadsCount = 10,
                      std::string output = "allLinks.txt") :
-            startingPoint(std::move(beginPage)), depth(maxDepth),
+            startingPoint(std::move(beginPage)), //std::move() - конвертирует передаваемый аргумент в r-value (конструктор перемещения)
+            depth(maxDepth),
             networkThreadsCount(producerThreadsCount),
             parserThreadsCount(consumerThreadsCount),
             outputPath(std::move(output)) {
     }
 
     void handler() {
-        HrefData fatherOfAll{startingPoint, 0};
-        hrefQueue.push(fatherOfAll);    //добавление в очередь типа HrefData (ссылка и уровень)
-        imgQueue.push(startingPoint);   //добавление в очередь типа string
-        boost::thread_group hrefFabric;
-        boost::thread_group imgFabric;
+        HrefData fatherOfAll{startingPoint, 0};//создание объекта структуры и инициализируем link и rang
+        hrefQueue.push(fatherOfAll); //добавление в очередь типа HrefData (ссылка и глубины)
+        imgQueue.push(startingPoint);//добавление в очередь типа string url
+        boost::thread_group hrefFabric;//создание группы потоков для скачивания страниц
+        boost::thread_group imgFabric;// создание группы потоков для обработок ссылок с фото
         for (uint8_t i = 0; i < networkThreadsCount; ++i)
-            hrefFabric.create_thread(boost::bind(&Crawler::
-            hrefWorker, this, i));  // создание i-го потока для СКАЧИВАНИЯ СТРАНИЦЫ
-
-        hrefFabric.join_all();  // включение потоков для скачивания
+            hrefFabric.create_thread(boost::bind(&Crawler::hrefWorker, this, i));  // создание i-го потока для СКАЧИВАНИЯ СТРАНИЦЫ
+//создаем поток, в который передаем метод hrefWorker для текущего объекта с id = i
+//bind — шаблонная функция, возвращающая объект, который можно вызвать, передав ему необходимое число аргументов в круглых скобочках
+        hrefFabric.join_all();// включение потоков для скачивания
         for (uint8_t i = 0; i < parserThreadsCount; ++i)
-            imgFabric.create_thread(boost::bind(&Crawler::imgWorker, this)); // создание i-го потока для ПРОЦЕССИНГА СТРАНИЦЫ
-        imgFabric.join_all();   // включение потоков для процессинга
+            imgFabric.create_thread(boost::bind(&Crawler::imgWorker, this));// создание i-го потока для ПРОЦЕССИНГА СТРАНИЦЫ(извлечение фото)
+        imgFabric.join_all();// включение потоков для процессинга
     }
 
     void hrefWorker(uint16_t id) {
-        while (true) {  // бесконечный цикл
+        while (true) { // бесконечный цикл
             try {
                 hrefMuter.lock();   // блокируем текущий поток (только "я" сейчас могу его использовать)
-                if (!hrefQueue.empty()) {   // если очередь пустая
+                if (!hrefQueue.empty()) {   // если очередь НЕ пустая
                     HrefData href = hrefQueue.front();  // обращение к первому элементу очереди
                     hrefQueue.pop();    // удалить первый элемент
                     hrefMuter.unlock(); //  освобождаем поток
-                    if (href.link.empty()) continue;    // если ссылки нет, то на на 87 строку??
+                    if (href.link.empty()) continue; // если ссылки нет, то переходим в конец тела цикла
 
                     std::string page = getPage(href.link);  // в page хранится весь код страницы
 
-                    if (page.empty())continue;  // если код станицы пустрой то на 87 строку
-                    getLinks(fromStrToNode(page)->root, href);  // достаем из page все ссылки
-                    hrefMuter.lock();
-                    std::cout << id << ": " << href.link <<
-                              " - " << href.rang << std::endl;
-                    hrefMuter.unlock();
+                    if (page.empty())continue;  // если код стpаницы пустой, то переходим в конец тела цикла
+                    getLinks(fromStrToNode(page)->root, href);// достаем из page все ссылки //парсит HTML страницы на ссылки
+                    //root- тег HTML,который формирует корень документа
+                    hrefMuter.lock();// блокируем текущий поток
+                    std::cout << id << ": " << href.link << " - " << href.rang << std::endl;
+                    hrefMuter.unlock();//  освобождаем поток
                 } else {
                     hrefMuter.unlock();
                     break;
@@ -96,16 +99,16 @@ public:
         }
     }
 
-    void imgWorker() {  // работка с ссылками на страницу
+    void imgWorker() {
         while (true) {
             try {
-                imgMuter.lock();
-                if (!imgQueue.empty()) {
-                    std::string img = imgQueue.front();
-                    imgQueue.pop();
-                    imgMuter.unlock();
+                imgMuter.lock();//блокируем поток
+                if (!imgQueue.empty()) {//если очередь не пуста
+                    std::string img = imgQueue.front();//заносим первый элемент очереди в img
+                    imgQueue.pop();//удаляем этот элемент
+                    imgMuter.unlock();//освобождаем поток
                     if (img.empty()) continue;
-                    std::string page = getPage(img);
+                    std::string page = getPage(img);//парсинг страницы
                     if (page.empty())continue;
                     getImg(fromStrToNode(page)->root);
                 } else {
@@ -121,11 +124,11 @@ public:
     }
 
     static GumboOutput *fromStrToNode(std::string &str) {
-        GumboOutput *output = gumbo_parse(str.c_str());
+        GumboOutput *output = gumbo_parse(str.c_str());//парсит HTML страницы на ссылки
         return output;
     }
 
-    void getLinks(GumboNode *node, const HrefData &parent) {    // достаем из кода страницы все ссылки
+    void getLinks(GumboNode *node, const HrefData &parent) { // достаем из кода страницы все ссылки
         try {
             if (node->type != GUMBO_NODE_ELEMENT) {
                 return;
@@ -150,6 +153,7 @@ public:
                 }
             }
             GumboVector *children = &node->v.element.children;
+            //Программа должна рекурсивно обойти все ссылки встречающиеся на странице.
             for (unsigned int i = 0; i < children->length; ++i) {
                 getLinks(static_cast<GumboNode *>(children->data[i]), parent);
             }
@@ -157,7 +161,7 @@ public:
         catch (...) { return; }
     }
 
-    void getImg(GumboNode *node) {  // работа с ссылками на картинки и сохранение этих ссылков файл
+    void getImg(GumboNode *node) {
         try {
             if (node->type != GUMBO_NODE_ELEMENT) {
                 return;
@@ -170,16 +174,17 @@ public:
                 std::string s = img->value;
                 if (s.find("http") == 0) {
                     imgMuter.lock();
-                    std::ofstream out(outputPath, std::ios::app);
+                    std::ofstream out(outputPath, std::ios::app);//создается поток для записи в файл
                     if (out.is_open()) {
                         // std::cout<<"IMAGE: "<<s<<std::endl;
-                        out << "IMAGE: " << s << std::endl;
+                        out << "IMAGE: " << s << std::endl; //запись в файл
                     }
                     out.close();
                     imgMuter.unlock();
                 }
             }
             GumboVector *children = &node->v.element.children;
+            //Программа должна рекурсивно обойти все ссылки встречающиеся на странице.
             for (unsigned int i = 0; i < children->length; ++i) {
                 getImg(static_cast<GumboNode *>(children->data[i]));
             }
@@ -190,8 +195,8 @@ public:
     static std::string getPage(std::string url) {
         std::string page;
         if (getPort(url) == "80") { // http
-            page = getHttp(url);
-        } else { page = getHttps(url); }    //https
+            page = getHttp(url);//скачается http страница
+        } else { page = getHttps(url); }  // скачается https(443) страница
         return page;    // возврат страницы
     }
 
@@ -215,17 +220,18 @@ public:
             http::response<http::dynamic_body> res;
             http::read(stream, buffer, res);
             return boost::beast::buffers_to_string(res.body().data());
-            // все что выше-программа скачивает страницу и возвращает эту строку
+            // все что выше-программа скачивает исходный код страницу и возвращает эту строку
         }
         catch (...) {
             return "";
         }
     }
 
+
     static std::string getHttps(std::string &url) {
-        std::string const host = getHost(url);
+        std::string const host = getHost(url);// получаем хост
         std::string const port = "443"; // https - 443, http - 80
-        std::string const target = getTarget(url);
+        std::string const target = getTarget(url); // получаем строку после хоста
         int version = 11; // или 10 для http 1.0
         try {
             boost::asio::io_context ioc;
@@ -253,13 +259,13 @@ public:
             http::response<http::dynamic_body> res;
             http::read(stream, buffer, res);
             return boost::beast::buffers_to_string(res.body().data());
-            // все что выше-программа скачивает страницу и возвращает эту строку
+            // все что выше-программа скачивает исходный код страницу и возвращает эту строку
         } catch (...) {
             return "";
         }
     }
 
-    static std::string getHost(std::string &url) {   // поиск хоста
+    static std::string getHost(std::string &url) { // поиск хоста
         /*
          пример:
          входные    url  =   https://support.microsoft.com/ru-ru/help/972034/how-to-reset-the-hosts-file-back-to-the-default
@@ -269,39 +275,40 @@ public:
         int64_t skipHTTP = 0;
         int64_t skipHTTPS = 0;
         int64_t pos = 0;
-        skipHTTPS = url.find("https");
-        skipHTTP = url.find("http");
-        if (skipHTTPS != -1)pos += skipHTTPS + 3 + 5;
-        else if (skipHTTP != -1)pos += skipHTTP + 3 + 4;
-        int64_t endOfHost = url.find('/', pos);
-        if (endOfHost == -1)endOfHost = url.size();
+        skipHTTPS = url.find("https");//позиция строки https, если нет строки то -1
+        skipHTTP = url.find("http");//позиция строки http
+        if (skipHTTPS != -1)pos += skipHTTPS + 3 + 5;//смещаемся на https://
+        else if (skipHTTP != -1)pos += skipHTTP + 3 + 4;//смещаемся на http://
+        int64_t endOfHost = url.find('/', pos);//позиция / после смещения
+        if (endOfHost == -1)endOfHost = url.size();//запишем размер url
         for (int64_t i = pos; i < endOfHost; ++i)
-            host.push_back(url[i]);
+            host.push_back(url[i]);//по буквам заносим в host
         return host;
     }
 
     static std::string getTarget(std::string &url) {
         /*
          пример:
-         входные    url  =   https://support.microsoft.com/ru-ru/help/972034/how-to-reset-the-hosts-file-back-to-the-default
-         получилось host =   /ru-ru/help/972034/how-to-reset-the-hosts-file-back-to-the-default/
+         входные    url  =   https://www.cyberforum.ru/cpp-beginners/thread2340562.html
+         получилось target =  /cpp-beginners/thread2340562.html/
          */
         std::string target;
-        int64_t www = url.find("www");
+        int64_t www = url.find("www"); //позиция строки www
         int64_t skipWWW = 0;
-        if (www != -1) skipWWW = www + 2;
+        if (www != -1) skipWWW = www + 2;//смещаемся на https://www.
         int64_t endOfHost = url.find('.', skipWWW);
         int64_t targetStartPos = url.find('/', endOfHost);
         for (uint64_t i = targetStartPos; i < url.size(); ++i) {
-            target.push_back(url[i]);
+            target.push_back(url[i]);//по буквам заносим в target
         }
-        if (target[target.size() - 1] != '/') target.push_back('/');
+        if (target[target.size() - 1] != '/') target.push_back('/');//добавится в конец '/'
         return target;
     }
 
-    static std::string getPort(std::string &url) {  // проверка ссылки, какой использовать порт
-        int64_t portFlag = url.find("https");
-        if (portFlag != -1) return "443";   //Если ссылка начинается на https (зашифр трафик), то 443 порт
+    // статические методы не привязаны к объекту
+    static std::string getPort(std::string &url) { // проверка ссылки, какой использовать порт
+        int64_t portFlag = url.find("https");//вернет номер подстроки в которой встретиться https, если не находит то код ошибки -1
+        if (portFlag != -1) return "443";  //Если ссылка начинается на https (зашифр трафик), то 443 порт
         return "80";    // Иначе 80 порт, http (незашифр трафик)
     }
 
@@ -313,7 +320,7 @@ public:
     std::string outputPath;
 
 private:
-    std::queue<HrefData> hrefQueue;
+    std::queue<HrefData> hrefQueue; //queue - очередь
     std::queue<std::string> imgQueue;
     boost::mutex hrefMuter;
     boost::mutex imgMuter;
--- 9/source.cpp
+++ 9/source.cpp
@@ -1,16 +1,33 @@
 // Copyright 2018 Your Name <your_email>
 
-#include <header.hpp>
+#include "header.hpp"
 
-int main(int argc, char **argv) {
-    if (argc) {             // обработка параметров командной строки
-        std::string url = argv[0];
-        uint64_t depth = std::stoi(argv[1]);
-        uint64_t network_threads = std::stoi(argv[2]);
-        uint64_t parser_threads = std::stoi(argv[3]);
-        std::string output = argv[4];
-        Crawler s(url, depth, network_threads, parser_threads, output);
-        s.handler();
+//Передача параметров при запуске из командной строки
+int main(int argc, char **argv) { //int argc (5) - количество передаваемых парметров, char **argv -  массив указателей на строки
+
+    boost::program_options::options_description desc("asd");//создаем экзампляр desc класса options_description
+    desc.add_options() //задаем опции
+            ("url", boost::program_options::value<std::string>())
+            ("depth", boost::program_options::value<unsigned>())
+            ("network_threads", boost::program_options::value<unsigned>())
+            ("parser_threads", boost::program_options::value<unsigned>())
+            ("output", boost::program_options::value<std::string>());
+    boost::program_options::variables_map vm;//создаем словарь vm
+    try { //парсим строку
+        boost::program_options::store(boost::program_options::
+                                      parse_command_line(argc, argv, desc), vm);
+        boost::program_options::notify(vm);
     }
+    catch (boost::program_options::error &e) { //обработка ошибки
+        std::cout << e.what() << std::endl;
+    }
+    
+    Crawler s(vm["url"].as<std::string>(),
+              vm["depth"].as<unsigned>(),
+              vm["network_threads"].as<unsigned>(),
+              vm["parser_threads"].as<unsigned>(),
+              vm["output"].as<std::string>());
+    s.handler();
+
     return 0;
 }
